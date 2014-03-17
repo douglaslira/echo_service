3427086a7412df4a03c3c0998e4af4fe2f877bbe
Supun Nakandala <supun.nakandala@gmail.com>
2014-03-17 23:16:40 +0530

    Added a service wrapper for echo service js client with promises

--- WebContent/Web/app/js/controllers.js
+++ WebContent/Web/app/js/controllers.js
@@ -3,11 +3,17 @@
 
 /* Controllers */
 angular.module('myApp.controllers', []).
-  controller('echoServiceController', ['$scope','echo_client',function($scope, echo_client) {
+  controller('echoServiceController', ['$scope', 'echo_client',function($scope, echo_client) {
 	  $scope.input = {text : "Hello World"};
 	  $scope.output = {text : ""};
 	  
-	  $scope.echo = function() {		  
-		  $scope.output.text = echo_client.echo($scope.input.text);		  
+	  $scope.echo = function() {
+		  echo_client.echo($scope.input.text).then(function(message){
+			  $scope.output.text = message;
+			  if(!$scope.$$phase) {
+				  $scope.$apply();
+			  }
+		  });
+		  		  
       };
  }]);
--- WebContent/Web/app/js/services.js
+++ WebContent/Web/app/js/services.js
@@ -2,12 +2,15 @@
 
 /* Services */
 angular.module('myApp.services', []).
-factory('echo_client',function($window) {		
+factory('echo_client',function($window, $q) {		
 	var EchoClient = $window.EchoClient;
     return {
         echo: function(message) {     	
+        	var defered = $q.defer();
         	var result = EchoClient.echo(message);
-            return result;
+        	defered.resolve(result);
+        	
+            return defered.promise;
         }
     };
   return EchoClient;
